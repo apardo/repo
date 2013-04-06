c08875f068fa2b52d8d8db28a2fc608ab5c4d101
Antonio Pardo Sánchez <apardo@alabs.org>
2013-04-06 14:31:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repo::Application.config.secret_token = 'd00d49fbfe3f82f78c851c5b1fb71a6b56dcfb181149d4c8981c7f87728d557ae2373435372481a0a883ce7f7e6e38400ec75f2a8563f404c178af61b985ebf9'
